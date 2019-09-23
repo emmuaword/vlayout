567f7e1aaa2b100f1841554f343e82de424462bf
tangwei <2278671454@qq.com>
2019-09-23 12:09:49 +0800

    适配androidx

--- vlayout/src/main/java/com/alibaba/android/vlayout/layout/RangeGridLayoutHelper.java
+++ vlayout/src/main/java/com/alibaba/android/vlayout/layout/RangeGridLayoutHelper.java
@@ -24,11 +24,11 @@
 
 package com.alibaba.android.vlayout.layout;
 
-import android.support.annotation.NonNull;
-import android.support.v4.util.ArrayMap;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.RecyclerView.Recycler;
-import android.support.v7.widget.RecyclerView.State;
+import androidx.annotation.NonNull;
+import androidx.collection.ArrayMap;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.Recycler;
+import androidx.recyclerview.widget.RecyclerView.State;
 import android.util.Log;
 import android.view.View;
 
@@ -43,7 +43,7 @@ import com.alibaba.android.vlayout.layout.GridLayoutHelper.SpanSizeLookup;
 
 import java.util.Arrays;
 
-import static android.support.v7.widget.LinearLayoutManager.VERTICAL;
+import static androidx.recyclerview.widget.LinearLayoutManager.VERTICAL;
 
 /**
  * LayoutHelper provides RangeGridLayoutHelper. The difference with {@link GridLayoutHelper} is that this layoutHelper could has child group logically but implemented as flat.
